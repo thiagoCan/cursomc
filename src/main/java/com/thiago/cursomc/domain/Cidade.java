ff73c34728213e31c440de681feadf10ec8842d7
Thiago Canonici <t_canonici@hotmail.com>
2022-06-02 20:42:43 -0300

    Endpoint /clientes/{id} disponivel

--- src/main/java/com/thiago/cursomc/domain/Cidade.java
+++ src/main/java/com/thiago/cursomc/domain/Cidade.java
@@ -10,6 +10,8 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 @Entity
 public class Cidade implements Serializable {
 	private static final long serialVersionUID = 1L;
@@ -20,6 +22,7 @@ public class Cidade implements Serializable {
 	private String nome;
 	
 	//cardinalidade 1: uma cidade pertence a um estado.
+	@JsonManagedReference
 	@ManyToOne
 	@JoinColumn(name = "estado_id")
 	private Estado estado;
--- src/main/java/com/thiago/cursomc/domain/Cliente.java
+++ src/main/java/com/thiago/cursomc/domain/Cliente.java
@@ -15,6 +15,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.thiago.cursomc.domain.enums.TipoCliente;
 
 @Entity
@@ -29,6 +30,7 @@ public class Cliente implements Serializable {
 	private String cpfOuCnpj;
 	private Integer tipo;
 	
+	@JsonManagedReference
 	@OneToMany(mappedBy = "cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
 	
